c70d3da687a2db2dfc5fa3cb9075b113f8338e97
loch704 <35868202+loch704@users.noreply.github.com>
2026-07-21 15:01:12 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -3,7 +3,7 @@ import requests
 BOT_TOKEN = "8872232866:AAFrLRFygu-w7f2lrPCPeeZtZoAX-2VavuE"
 CHAT_ID = "354370983"
 
-url = "https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
 payload = {
     "chat_id": CHAT_ID,
