86eea34536e3dfef73d9d03a28143dd8df6f81fc
loch704 <35868202+loch704@users.noreply.github.com>
2026-07-21 15:18:02 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -5,15 +5,15 @@ url = "https://lastchancetoy.com/products.json"
 data = requests.get(url).json()
 
 KEYWORDS = [
-    "Arranged Wigs",
+    "arranged wigs",
 ]
 
-#for product in data["products"]:
-
-#    title = product["title"]
-
-#    if any(k in title.lower() for k in KEYWORDS):
-#        print(title)
-
 for product in data["products"]:
-    print(product["title"])
+
+    title = product["title"]
+
+    if any(k in title.lower() for k in KEYWORDS):
+        print(title)
+
+#for product in data["products"]:
+#    print(product["title"])
