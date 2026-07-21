74cab14333750e4001f740f9df41bdf006e4e00d
loch704 <35868202+loch704@users.noreply.github.com>
2026-07-21 15:16:11 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -8,9 +8,12 @@ KEYWORDS = [
     "Arranged Wigs",
 ]
 
+#for product in data["products"]:
+
+#    title = product["title"]
+
+#    if any(k in title.lower() for k in KEYWORDS):
+#        print(title)
+
 for product in data["products"]:
-
-    title = product["title"]
-
-    if any(k in title.lower() for k in KEYWORDS):
-        print(title)
+    print(product["title"])
