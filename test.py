8907794ac93a4f24cfaf26949ee4f85b020189a5
loch704 <35868202+loch704@users.noreply.github.com>
2026-07-21 16:59:59 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,26 @@
 import requests
+import json
 
-url = "https://www.hobbylandeshop.com/"
+url = "https://backend.hobbylandeshop.com/api/products"
 
-html = requests.get(
-    "https://www.hobbylandeshop.com/"
-).text
+payload = {
+    "page": 1,
+    "category": ["nproduct_booking"],
+    "stockStatus": "in_stock"
+}
 
-print(html)
+headers = {
+    "Origin": "https://www.hobbylandeshop.com",
+    "Referer": "https://www.hobbylandeshop.com/",
+    "User-Agent": "Mozilla/5.0",
+    "Content-Type": "application/json"
+}
+
+response = requests.post(
+    url,
+    json=payload,
+    headers=headers
+)
+
+print(response.status_code)
+print(response.text[:5000])
