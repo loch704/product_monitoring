8461cd9cac4a8c7fded58c764139927d89cb469d
loch704 <35868202+loch704@users.noreply.github.com>
2026-07-21 16:53:51 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 import requests
-from bs4 import BeautifulSoup
 
 url = "https://www.hobbylandeshop.com/"
 
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
-
-html = requests.get(url, headers=headers).text
+html = requests.get(
+    "https://www.hobbylandeshop.com/"
+).text
 
 print(html)
