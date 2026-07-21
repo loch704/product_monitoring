be7281d313d47ab247ece2f61731cafb24764c27
loch704 <35868202+loch704@users.noreply.github.com>
2026-07-21 15:26:32 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -13,7 +13,18 @@ for product in data["products"]:
     title = product["title"]
 
     if any(k in title.lower() for k in KEYWORDS):
-        print(title)
+        message = f"""
+		🚨 New Product Found
+        {title}https://lastchancetoy.com/products/{product['handle']}
+        """
+		
+		requests.post(
+			f"https://api.telegram.org/bot8872232866:AAFrLRFygu-w7f2lrPCPeeZtZoAX-2VavuE/sendMessage",
+			data={
+				"chat_id": 354370983,
+				"text": message
+    }
+)
 
 #for product in data["products"]:
 #    print(product["title"])
