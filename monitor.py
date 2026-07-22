e84896322a63fbdfabe874e235b2550993b9d902
loch704 <35868202+loch704@users.noreply.github.com>
2026-07-22 15:37:30 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -7,6 +7,89 @@ print(f"[{datetime.now()}] Monitor Started")
 BOT_TOKEN = "YOUR_TOKEN"
 CHAT_ID = "YOUR_CHAT_ID"
 
+def send_telegram(message):
+
+    requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        data={
+            "chat_id": CHAT_ID,
+            "text": message
+        }
+    )
+
+def monitor_lastchance(known_products):
+
+    print(f"[{datetime.now()}] Checking LastChanceToy")
+
+    data = requests.get(
+        "https://lastchancetoy.com/products.json"
+    ).json()
+
+    for product in data["products"]:
+
+        title = product["title"]
+
+        if not any(
+            k.lower() in title.lower()
+            for k in KEYWORDS
+        ):
+            continue
+
+        product_id = f"lastchance|{product['id']}"
+
+        if product_id in known_products:
+            continue
+
+        message = f"""
+🚨 New Product Found
+
+Source: LastChanceToy
+
+{title}
+
+https://lastchancetoy.com/products/{product['handle']}
+"""
+
+        send_telegram(message)
+
+        known_products.add(product_id)
+
+        print(
+            f"[{datetime.now()}] Notify: {title}"
+        )
+
+def main():
+
+    known_products = set()
+
+    if os.path.exists("notified_products.txt"):
+        with open(
+            "notified_products.txt",
+            "r",
+            encoding="utf-8"
+        ) as f:
+
+            known_products = {
+                line.strip()
+                for line in f
+            }
+
+    monitor_lastchance(known_products)
+    #monitor_hobbyland(known_products)
+
+    with open(
+        "notified_products.txt",
+        "w",
+        encoding="utf-8"
+    ) as f:
+
+        for item in sorted(known_products):
+            f.write(item + "\n")
+
+if __name__ == "__main__":
+    main()
+
+'''
 URL = "https://lastchancetoy.com/products.json"
 
 KEYWORDS = [
@@ -77,3 +160,4 @@ if new_products:
             f.write(item + "\n")
 
 print(f"[{datetime.now()}] Monitor Finished")
+'''
